434222f756f42a2cfeb354109e9f0489e90ea4de
Terentyev Vladimir <Terentyev_Vladimir@ukr.net>
2023-03-04 16:41:44 +0200

    ДЗ №6 - 2 задача

--- js/task-02.js
+++ js/task-02.js
@@ -7,36 +7,20 @@ const ingredients = [
   "Condiments",
 ];
 
+const listEl = document.querySelector("#ingredients");
 
-const listEl = document.querySelector('.ingredients');
-
-ingredients.forEach((ingredients) => {
+const arrayEl = ingredients.map((ingredients) => {
   const listItemEl = document.createElement("li");
- 
-  listItemEl.textContent = ingredients;
+  
   listItemEl.classList.add("item");
+  listItemEl.textContent = ingredients;
   console.log(listItemEl);
+  return listItemEl;
 });
 
-const array = ingredients.map((el) => ingredients(el));
-// return el.listItemEl;
-console.log(array)
-
-
-// const arrayEl = ingredients.map((ingredient) => ingredient.listItemEl);
-// console.log(arrayEl);
-
-// listEl.append(listItemEl);
+console.log(arrayEl);
+listEl.append(...arrayEl);
 
 
 
-
-
-
-
-// const listEl = document.querySelector(".ingredients");
-// const listItemEl = document.createElement('li');
-// listItemEl.classList.add("item");
-// listItemEl.textContent = "Potatoes";
-// console.log(listItemEl);
 
