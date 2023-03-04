1b399944aa7286f6654c2a6f8717fff0c60bfb8c
Terentyev Vladimir <Terentyev_Vladimir@ukr.net>
2023-03-04 15:40:22 +0200

    ДЗ №6 - 1 задача

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,30 @@
-const nameEs = document.querySelector('.categories');
-console.log(nameEs);
+//All element
+
+const AllCategoriesEl = categories.querySelectorAll(".item");
+console.log('Number of categories:', AllCategoriesEl.length);
+
+
+//First element
+
+const animalsEl = categories.firstElementChild;
+console.log("Category:", animalsEl.firstElementChild.textContent);
+
+const animalsLastEl = animalsEl.lastElementChild;
+console.log("Elements:", animalsLastEl.children.length);
+
+//Second element
+
+const productsEl = categories.firstElementChild.nextElementSibling;
+console.log("Category:", productsEl.firstElementChild.textContent);
+
+const productsLastEl = productsEl.lastElementChild;
+console.log("Elements:", productsLastEl.children.length);
+
+//Last element
+
+const technologiesEl = categories.lastElementChild;
+console.log("Category:", technologiesEl.firstElementChild.textContent);
+
+const technologiesLastEl = technologiesEl.lastElementChild;
+console.log("Elements:", technologiesLastEl.children.length);
+
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,42 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+
+const listEl = document.querySelector('.ingredients');
+
+ingredients.forEach((ingredients) => {
+  const listItemEl = document.createElement("li");
+ 
+  listItemEl.textContent = ingredients;
+  listItemEl.classList.add("item");
+  console.log(listItemEl);
+});
+
+const array = ingredients.map((el) => ingredients(el));
+// return el.listItemEl;
+console.log(array)
+
+
+// const arrayEl = ingredients.map((ingredient) => ingredient.listItemEl);
+// console.log(arrayEl);
+
+// listEl.append(listItemEl);
+
+
+
+
+
+
+
+// const listEl = document.querySelector(".ingredients");
+// const listItemEl = document.createElement('li');
+// listItemEl.classList.add("item");
+// listItemEl.textContent = "Potatoes";
+// console.log(listItemEl);
+
